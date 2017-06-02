bfac2e4f2bd524e0e9f5da606bbb505bd933c8b2
yupengzte <yu.peng36@zte.com.cn>
2017-06-02 09:58:27 +0800

    add codebeat coverage

---
+++ run_CI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+shopt -s globstar
+rootdir=`pwd`
+
+for dir in **/ ; do
+  cd $dir
+  exec 5>&1
+  out=$(go test -v -coverprofile=cov_part.out | tee >(cat - >&5))
+  if [ $? -eq 1 ] ; then
+    if [ $(cat $out | grep -o 'no buildable Go source files') == "" ] ; then
+      echo "Tests failed! Exiting..." ; exit 1
+    fi
+  fi
+  cd $rootdir
+done
+
+if [ -z "$CI_NAME" ]; then
+  echo "CI_NAME is unset. Skipping coverage report!"
+  exit 0
+fi
+
+find . -name cov_part.out | xargs cat > cov.out
+# make sure we do not run the ruby gem which is first in $PATH :(
+node /usr/local/lib/node_modules/codeclimate-test-reporter/bin/codeclimate.js < cov.out
